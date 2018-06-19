824432868facaf153c2011f023986e0df9163638
VioletQQY <forviolet771@gmail.com>
2018-06-19 15:01:17 +0800

    add page reset password success

---
+++ app/components/Login/ResetSuccessComponent.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { List, WhiteSpace, WingBlank, Button } from 'antd-mobile'
+
+import './login.scss'
+ 
+class Index extends React.Component {
+  render() {
+    return (
+      <div className='full__container'>
+        <WhiteSpace size='lg' />
+        <WhiteSpace size='lg' />
+        <WingBlank size='lg' style={{lineHeight: '1.75', fontSize: '18px'}}>
+          <p>新密码已通过短信发至您注册的手机号码，请注意查收。登录后您可进入用户详情页面进行密码修改操作。</p>
+        </WingBlank>
+        <WhiteSpace size='lg' />
+        <WhiteSpace size='lg' />
+        <WingBlank size='lg'>
+          <Button type='primary'>返回登录</Button>
+        </WingBlank>
+        <WhiteSpace size='lg' />
+      </div>
+    )
+  }
+}
+
+export default Index
--- pages/login/resetsuccess.js
+++ pages/login/resetsuccess.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Head from '../../app/components/Common/Head'
+import ResetSuccessComponent from '../../app/components/Login/ResetSuccessComponent'
+
 class Index extends React.Component {
   static async getInitialProps(ctx) {
     console.log(ctx)
@@ -8,7 +11,10 @@ class Index extends React.Component {
 
   render() {
     return (
-      <div>Success!</div>
+      <div>
+        <Head title='修改密码成功' />
+        <ResetSuccessComponent />
+      </div>
     )
   }
 }
