a9041d72dd740da5914311a4ab233bfbf6757d4d
VioletQQY <forviolet771@gmail.com>
2018-06-20 15:15:49 +0800

    create flex list component & complete account component

---
+++ app/components/Common/FlexList.js
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import './flexlist.scss'
+
+const FlexListConfigInitial = {
+  leftWidth: '100px',
+  rightWidth: 0, 
+  minHeight: '30px',
+  withBorder: true
+}
+
+export class FlexListConfigEntity {
+  constructor(obj = FlexListInitial) {
+    this.leftWidth = obj.leftWidth
+    this.rightWidth = obj.rightWidth
+    this.minHeight = obj.minHeight
+    this.withBorder = obj.withBorder
+  }
+}
+
+export const FlexList = ({children, sub, extra, config}) => {
+  const getFlexClass = () => {
+    switch(config.withBorder) {
+      case 'border':
+        return 'flex__list flex__list__border'
+      case 'dash':
+        return 'flex__list flex__list__dash'
+      case 'href':
+        return 'flex__list flex__list__href'
+      default:
+       return 'flex__list'
+    }
+  }
+  return (
+    <div className={getFlexClass()} style={{minHeight: config.minHeight}}>
+      <div className='list__main'>
+        <div style={{margin: `0 ${config.rightWidth} 0 ${config.leftWidth}`}}>
+          {children}
+        </div>
+      </div>  
+      <div className='list__left' style={{marginLeft: '-100%', width: config.leftWidth}}>
+        {sub}
+      </div>  
+      <div className='list__right' style={{marginLeft: `-${config.rightWidth}`, width: config.rightWidth}}>
+        {extra}
+      </div>  
+    </div>
+  )
+}
+
+export const ImageContainer = ({imageUrl, containerPadding = '15px', imageClass}) => (
+  <div className='image__container' style={{padding: containerPadding}}>
+    <img src={imageUrl} className={imageClass} />
+  </div>
+)
+
+export const MainContainer = ({children, mainClass}) => (
+  <div className={mainClass}>
+    {children}
+  </div>
+)
--- app/components/Personal/AccountComponent.js
+++ app/components/Personal/AccountComponent.js
@@ -1,35 +1,89 @@
 import React from 'react'
-import { List, WhiteSpace } from 'antd-mobile'
+import { List, WhiteSpace, WingBlank, Button } from 'antd-mobile'
+
+import { FlexList, ImageContainer, MainContainer, FlexListConfigEntity } from '../Common/FlexList'
 
 import '../Appointment/appointment.scss'
 import './personal.scss'
 
 class Index extends React.Component {
+
   render() {
+    const img = 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png'
+    const configAccount = new FlexListConfigEntity({
+      leftWidth: '85px',
+      rightWidth: '35px', 
+      minHeight: '50px',
+      withBorder: false
+    })
+    const configFamily = new FlexListConfigEntity({
+      leftWidth: '80px',
+      rightWidth: '35px', 
+      minHeight: '100px',
+      withBorder: 'dash'
+    })
+    const configFamilyLast = new FlexListConfigEntity({
+      leftWidth: '80px',
+      rightWidth: '35px', 
+      minHeight: '100px',
+      withBorder: 'border'
+    })
     return (
       <div>
         <List>
-          <div className='mylist'>
-            <div className='list__main'>
-              <div className='list__content user__desc'>
-                <p className='name'>石菁</p>
-                <p>身份证号：1234567887654321</p>
-                <p>联系电话：12334545</p>
-              </div>
-            </div>
-            <div className='list__left'>
-              <div className='doctor__avatar'>
-                <img src='https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png' />
-              </div>
-            </div>
-          </div>
+          <FlexList 
+            sub={<ImageContainer imageUrl={img} imageClass='user__avatar' containerPadding='18px 12px' />}
+            extra={<i className='anticon icon-right user__arraw' />}
+            config={configAccount}>
+            <MainContainer mainClass='user__desc'>
+              <p className='name'>石菁</p>
+              <p>身份证号：123445678765434567</p>
+              <p>联系电话：12334545</p>
+            </MainContainer>
+          </FlexList>
         </List>
         <WhiteSpace />
         <List>
-          <div>
-            123
-          </div>
+          <List.Item thumb={<i className='anticon icon-user user__tipicon' />}>
+            我的家庭成员
+          </List.Item>
         </List>
+        <FlexList 
+          sub={<MainContainer mainClass='user__accounts'><p>姓名</p><p>身份证</p><p>卡号</p></MainContainer>}
+          extra={<i className='anticon icon-right user__arraw' />}
+          config={configFamily}>
+          <MainContainer mainClass='user__accounts'>
+            <p>石菁</p>
+            <p>123445678765434567</p>
+            <p>12334545</p>            
+          </MainContainer>
+        </FlexList>
+        <FlexList 
+          sub={<MainContainer mainClass='user__accounts'><p>姓名</p><p>身份证</p><p>卡号</p></MainContainer>}
+          extra={<i className='anticon icon-right user__arraw' />}
+          config={configFamilyLast}>
+          <MainContainer mainClass='user__accounts'>
+            <p>石菁</p>
+            <p>123445678765434567</p>
+            <p>12334545</p>
+          </MainContainer>
+        </FlexList>
+        <WhiteSpace />
+        <List style={{border: 0}}>
+          <List.Item thumb={<i className='anticon icon-exclamationcircle user__tipicon' />} arrow='down'>
+            家庭卡添加说明
+          </List.Item>
+        </List>
+        <WhiteSpace size='lg' />
+        <WingBlank>
+          <Button type='primary'>添加家庭关系</Button>
+        </WingBlank>
+        <WhiteSpace size='lg' />
+        <WhiteSpace size='lg' />
+        <WingBlank>
+          <p className='user__tips'>如果您的信息已通过其他途径修改，请重新登录。</p>
+        </WingBlank>
+        <WhiteSpace size='lg' />
       </div>
     )
   }
