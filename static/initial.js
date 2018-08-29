bc0a5dcae6ba8dfa19ef26af601d8cbf3df3f661
VioletQQY <forviolet771@gmail.com>
2018-08-29 16:20:05 +0800

    add v console to debug

--- static/initial.js
+++ static/initial.js
@@ -6,3 +6,7 @@ if ('addEventListener' in document) {
 if (!window.Promise) {
   document.writeln('<script src="https://as.alipayobjects.com/g/component/es6-promise/3.2.2/es6-promise.min.js"></script>')
 }
+
+// init vConsole
+var vConsole = new VConsole();
+console.log('Hello world');
