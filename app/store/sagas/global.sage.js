e0239ba007b7cfb5eca71e6e683def5c5d336195
VioletQQY <forviolet771@gmail.com>
2018-08-29 16:29:25 +0800

    modify load jssdk

--- app/store/sagas/global.sage.js
+++ app/store/sagas/global.sage.js
@@ -1,4 +1,5 @@
 import { put, takeLatest, call, select } from 'redux-saga/effects'
+import { Toast } from 'antd-mobile'
 
 import { actionTypes, updateGlobalLocation } from '../actions/global.action'
 import { HttpUnSecretService } from '../../utilities/httpService'
@@ -33,6 +34,11 @@ function* loadJssdk() {
         window.wx.ready(() => {
           window.wx.hideAllNonBaseMenuItem() 
         })
+        wx.error((res) => {
+          console.log(res)
+          Toast.info('微信JS-SDK调用失败')
+          // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+      });
       }
     }
   } catch (error) {
