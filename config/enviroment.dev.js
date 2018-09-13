d6db0b0a64abe6b223cfa5c0d6fba8cbd0f3bd93
VioletQQY <forviolet771@gmail.com>
2018-09-13 17:07:29 +0800

    modify

--- config/enviroment.dev.js
+++ config/enviroment.dev.js
@@ -1,5 +1,6 @@
 module.exports = {
-  apiUrl: 'http://wechat-yilian.medicine-tec.cn',
+  apiUrl: 'https://shdcapp.wondersgroup.com/yilian-cloud-api',
+  // apiUrl: 'http://wechat-yilian.medicine-tec.cn',
   domain: 'shdc.violetqqy.com',
   appId: 'wx56d7123f5687bfe8',
   defaultPort: 3009
