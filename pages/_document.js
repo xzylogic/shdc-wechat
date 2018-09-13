65db906063619f1f8c6f82c026fc09c823842e00
VioletQQY <forviolet771@gmail.com>
2018-09-13 10:35:43 +0800

    remove vconsole

--- pages/_document.js
+++ pages/_document.js
@@ -21,7 +21,7 @@ export default class MyDocument extends Document {
           <script src='https://as.alipayobjects.com/g/component/fastclick/1.0.6/fastclick.js' />
           <script src='https://api.map.baidu.com/api?v=2.0&ak=2g6Ot3jfb2OM20OVREbVdpmPE21Nmzon' />
           <script src='https://res.wx.qq.com/open/js/jweixin-1.2.0.js' />
-          <script src='/static/vconsole.min.js' />
+          {/* <script src='/static/vconsole.min.js' /> */}
           <script src='/static/initial.js' />
         </Head>
         <body className='custom_class'>
