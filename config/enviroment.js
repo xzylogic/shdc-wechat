8ceba85981ba500b33763d4cc68689b0a345fbac
VioletQQY <forviolet771@gmail.com>
2018-09-13 10:06:00 +0800

    modify test environment

--- config/enviroment.js
+++ config/enviroment.js
@@ -1,6 +1,6 @@
 module.exports =  {
-  apiUrl: 'http://wechat-yilian.medicine-tec.cn',
-  domain: 'shdc.violetqqy.com',
+  apiUrl: 'https://shdcapp.wondersgroup.com/yilian-cloud-api',
+  domain: 'shdcapp.wondersgroup.com',
   appId: 'wx56d7123f5687bfe8',
   defaultPort: 3009
 }
